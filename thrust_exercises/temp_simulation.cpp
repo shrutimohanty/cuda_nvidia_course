81457014023872e0be074f14b5ce7d58f0b46b9f
Shruti Mohanty <shruti.my@gmail.com>
2025-11-08 12:54:37 -0500

    temp simulation

--- thrust_exercises/temp_simulation.cpp
+++ thrust_exercises/temp_simulation.cpp
@@ -7,11 +7,13 @@
 
 using namespace std;
 
-auto randnum = [=] (){
-    return 25 + ((rand()/RAND_MAX)*100 );
-};
+
 
 vector<float> init_temp_vector(int n){
+    auto randnum = [=] (float){
+    return 25 + ((rand()/RAND_MAX)*100 );
+    };
+
     vector<float> temp_init_values(n);
     transform(temp_init_values.begin(), temp_init_values.end(),temp_init_values.begin(),randnum);
 }
