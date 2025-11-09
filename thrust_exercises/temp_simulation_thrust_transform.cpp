c0f3f4d13df6c66150389cd0b82a028d0258e5bc
Shruti Mohanty <shruti.my@gmail.com>
2025-11-09 07:09:27 -0500

    temp simulation

--- thrust_exercises/temp_simulation_thrust_transform.cpp
+++ thrust_exercises/temp_simulation_thrust_transform.cpp
@@ -1,48 +1,40 @@
-#include <vector>
 #include <iostream>
-#include <vector>
-#include <random>
-#include <algorithm>
 #include <cstdlib>
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #include <thrust/transform.h>
-#include <thrust/functional.h>
-#include <iostream>
 
 using namespace std;
 
+float temp_room = 25;
+float k = 0.5;
 
-
-thrust::host_vector<float> init_temp_vector(int n){
-    auto randnum = [=] __host__(float){
-    return 25.0f + ((static_cast<float>(rand())/RAND_MAX)*75.0f);
-    };
-
-    thrust::host_vector<float> temp_init_values(n);
-    thrust::transform(temp_init_values.begin(), temp_init_values.end(),temp_init_values.begin(),randnum);
-
+vector<float> init_temp_vector(int n){
+    vector<float> temp_init_values(n);
+    for(int i =0; i<n; i++){
+        temp_init_values[i] = 25.0f + ((static_cast<float>(rand())/RAND_MAX)*75.0f);
+    }
     return temp_init_values;
 }
 
-void update_temp(vector<float>& temp_curr){
-    float k = 0.5f;
-    float temp_room = 25.0f;
-    auto op = [=] (float& temp_c){ return temp_c + k*(temp_room - temp_c);};
 
-    transform(temp_curr.begin(),temp_curr.end(),temp_curr.begin(),op);
-}
 
 int main(){
-    thrust::universal_vector<float> temp_values = init_temp_vector(3);
+    vector<float> temp_values = init_temp_vector(3);
 
     int iter = 4;
+
+    auto op = [=] (float temp_c){ 
+        float diff = temp_room - temp_c;
+        return temp_c + k*(diff);
+    };
 
     for(int i = 0; i< iter; i++){
         printf("Current temp vector:");
         for(float x:temp_values){ cout<<x<<" ";}
         cout<<endl;
-        update_temp(temp_values);
+
+        transform(temp_values.begin(),temp_values.end(),temp_values.begin(),op);
     }
 
 
--- vector_add/add.cpp
+++ vector_add/add.cpp
@@ -1,13 +1,55 @@
 #include <stdio.h>
 #include <vector>
+#include <thread>
+#include <malloc>
 
 using namespace std;
 
+vector<float> init_vector(int n){
+    vector<float> init_values(n);
+    for(int i =0; i<n; i++){
+        init_values[i] = 1;
+    }
+    return init_values;
+}
 
+void add_op(const float* A, const float* B, float* C, int start_idx, int stride, int n){
+    int i = 0;
+    while(start_idx<n && i < stride){
+        C[start_idx+i] = A[start_idx+i] + B[start_idx+i];
+    }
+    
+}
 
 int main(){
     int n = 1024;
-    vector<float> A(n), B(n), C(n);
 
+    vector<float> A = malloc(n*(sizeof(float)));
+    vector<float> B = malloc(n*(sizeof(float)));
+    vector<float> C = malloc(n*(sizeof(float)));
+
+    //initialize it
+    for (int i = 0; i<n; i++){
+        A[i] = 1;
+        B[i] = 2;
+        C[i] = 0;
+    }
+
+    int nproc = 8;
+    int thread_stride = (n+(nproc - 1))/nproc;
+
+    //create parallel works
+    vector<thread> threads;
+    for(int i = 0; i < nproc; i++){
+        int start_idx = i*thread_stride;
+        threads.emplace_back(add_op,A,B,C,start_idx,thread_stride,n)
+    }
+
+    //join parallel works
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    printf("C[0]=%.1f C[last]=%.1f\n", C.front(), C.back());
     return 1;
 }
